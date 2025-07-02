7c4bca0961ac51b479a213559a1f6c75818d0bb7
Miraj <mhmiraj2005@gmail.com>
2025-07-02 21:50:58 +0600

    removeed makefiles

--- bin/CMakeFiles/app.dir/utils/nPow.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/app.dir/utils/nPow.cpp.obj: \
- D:\INVENTORY\codx\cpp\CMake_Defaults\utils\nPow.cpp
--- bin/CMakeFiles/app.dir/utils/nRoot.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/app.dir/utils/nRoot.cpp.obj: \
- D:\INVENTORY\codx\cpp\CMake_Defaults\utils\nRoot.cpp
