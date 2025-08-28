88c9bc8d4af489ed24a5480e37c3199ca9f393dd
netchunk <mhmiraj2005@gmail.com>
2025-08-28 22:35:53 +0600

    minimalized

--- src/calc.cpp
+++ src/calc.cpp
@@ -4,13 +4,47 @@ const long double pi = 3.14159265358979323846;
 
 // sorry i made this repo's code more complex for beginners. u can use easy functions from other file by prototyping. example below
 
-long double nPow(long double base, int exp);     // this a function prototyping, the function exists in utils folder
-                                            // use mingw if you wanna dont wanna use namespace or functions name like 'abs' will conflict
-                                            // even if u didn't include cmath or any standard lib but clang stictly adds them or checks similarity
-                                            //                                      use mingw if u wanna avoid this issue
+long double nPow(long double base, int exp)
+{
+    long double result = 1;
+    for (int i = 0; i < exp; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+
 namespace math{
     long double abs(long double x);
     long double nRoot(long double base, int n);
+}
+
+long double math::abs(long double x) // returns the positive value if negative
+{
+    return (x < 0) ? -x : x;
+}
+
+long double math::nRoot(long double base, int n)
+{
+    long double low = 0;
+    long double high = base;
+    long double epsilion = 0.0000000000001;
+    long double guess;
+    int itt = 0;
+    int max_itt = 1000;
+
+    while(itt < max_itt)
+    {
+        guess = (low + high) / 2;
+        if( abs(nPow(guess, n)-base) < epsilion )
+            return guess;
+        else if(nPow(guess, n) < base)
+            low = guess;
+        else
+            high = guess;
+    }
+    return -1; // If no root is found within the iterations
+
 }
 
 int main(int argc, char* argv[])
@@ -68,8 +102,14 @@ int main(int argc, char* argv[])
 
 
 /*
-cmake -S . -B bin -G "MinGW Makefiles"
-cmake --build bin && bin\app.exe
+cmake -S . -B bin -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=g++
+            # -G "Unix Makefiles"
+            # -G "Ninja"
+            # -G "MinGW Makefiles"
+            # -G "Visual Studio 17 2022"
+cmake --build bin
+            # optional --target <target_name>
+                # --target run    {to compile and run}
+                # --target build_with_intermediates
 
-cmake --build bin && bin\app.exe 14 2
 */
